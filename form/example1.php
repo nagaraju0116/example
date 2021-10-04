851f8604d9588a626efa06dbc24775df1098a498
nagaraju0116 <nagaraju906k@gmail.com>
2021-10-04 16:08:22 +0530

    initial commit

---
+++ form/example1.php
@@ -0,0 +1,15 @@
+<?php
+$favourite="red";
+switch ($favourite)
+{
+     case "red":
+      echo "red is fav color";
+      break;
+     case "green":
+      echo "green is fav color";
+      break;
+     default :
+      echo " red is not fav color";
+}
+
+ ?>
